c72bf3b11a38d3c70abd61d26b3f631cae79b96e
Estebanmartinac1190 <92158255+Estebanmartinac1190@users.noreply.github.com>
2021-11-01 15:30:40 -0500

    Create Marca.java

---
+++ src/main/java/taller3/televisores/Marca.java
@@ -0,0 +1,20 @@
+package taller3.televisores;
+
+public class Marca {
+	//ATRIBUTOS
+	private String nombre;
+	
+	//CONSTRUCTOR
+	public Marca(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+}
